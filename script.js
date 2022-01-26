51116e6b75f2d949135e7c14004e7c239b0c69ad
Till-Taeubrich <Till.taeubrich@gmail.com>
2022-01-26 20:26:02 +1100

    added "hover-to-paint" function

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ const container = document.getElementById('container');
 
 // Functions
 
+window.onload = function(){
+  createGrid();
+}
+
 function createSquare() {
   let square = document.createElement('div');
   container.append(square);
   square.style.border = '1px solid black';
+  square.addEventListener('mouseenter', function(){
+    changeColor(square);
+  })
 }
 
 function createGrid() {
@@ -17,8 +24,10 @@ function createGrid() {
   container.style.setProperty('--grid-columns', SquareQuantityColumn);
 
   for (let i = 0; i < (SquareQuantityRow * SquareQuantityColumn); i++) {
-    createSquare()
+    createSquare();
   }
 }
 
-createGrid()
+function changeColor(e){
+  e.style.backgroundColor = 'green';
+}
