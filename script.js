f5796f6a429d5956e7841212f10e09ff50593582
Till-Taeubrich <Till.taeubrich@gmail.com>
2022-01-27 16:51:31 +1100

    Final Version
    Added button to change grid size. Created simple design.

--- script.js
+++ script.js
@@ -1,33 +1,63 @@
 // Variables
-const SquareQuantityRow = 5;
-const SquareQuantityColumn = SquareQuantityRow;
+const defaultSquareQuantityRow = 16;
+const defaultSquareQuantityColumn = defaultSquareQuantityRow;
 const container = document.getElementById('container');
+const clearButton = document.getElementById('clearButton');
+
+let SquareQuantityRow = defaultSquareQuantityRow;
+let SquareQuantityColumn = defaultSquareQuantityColumn;
 
 // Functions
-
 window.onload = function(){
   createGrid();
+}
+
+function createGrid() {
+  container.style.setProperty('--grid-rows', SquareQuantityRow);
+  container.style.setProperty('--grid-columns', SquareQuantityColumn);
+
+  for (let i = 0; i < (SquareQuantityRow * SquareQuantityColumn); i++) {
+  createSquare();
+  }
 }
 
 function createSquare() {
   let square = document.createElement('div');
   container.append(square);
   square.style.border = '1px solid black';
+
   square.addEventListener('mouseenter', function(){
     changeColor(square);
   })
 }
 
-function createGrid() {
-
-  container.style.setProperty('--grid-rows', SquareQuantityRow);
-  container.style.setProperty('--grid-columns', SquareQuantityColumn);
-
-  for (let i = 0; i < (SquareQuantityRow * SquareQuantityColumn); i++) {
-    createSquare();
-  }
-}
-
 function changeColor(e){
   e.style.backgroundColor = 'green';
 }
+
+function clearGrid(){
+  container.innerHTML = '';
+}
+
+function askForNewSize() {
+  SquareQuantityRow = prompt('How many squares per row/column? (Limit = 100)', '');
+  SquareQuantityColumn = SquareQuantityRow
+
+  for (;;) {
+    if (SquareQuantityRow >= 1 && SquareQuantityRow <= 100) {
+      createGrid();
+      break;
+    }
+    else {
+      alert('Wrong Input. Please select number between 1 - 100');
+      askForNewSize();
+    }
+  }
+}
+
+
+// Events
+clearButton.addEventListener('click', function() {
+clearGrid();
+askForNewSize();
+})
